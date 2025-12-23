2fc162a3e7abe95ddddb96a56f81dfd94379d32b
MatheusDeZottis <matheusdezottis@gmail.com>
2025-12-22 22:47:08 -0300

    feat Usando InjectRepository e Repository para ler e manipular a entity na tabela

--- src/recados/entities/recado.entety.ts
+++ src/recados/entities/recado.entety.ts
@@ -1,8 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('recados')
 export class RecadoEntity {
+    @PrimaryGeneratedColumn()
     public id: number;
+
+    @Column({type: 'varchar', length: 255})
     public texto: string;
+
+    @Column({type: 'varchar', length: 50})
     public de: string;
+
+    @Column({type: 'varchar', length: 50})
     public para: string;
+
+    @Column({type: 'boolean', default: false})
     public lido: boolean;
+    
+    @Column({type: 'date'})
     public data: Date;
+
+    @CreateDateColumn()
+    createdAt?: Date;
+
+    @CreateDateColumn()
+    updatedAt?: Date;
 }
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -2,83 +2,92 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { RecadoEntity } from "./entities/recado.entety";
 import { CreateRecadoDto } from "./DTO/create-recado.dto";
 import { UpdateRecadoDto } from "./DTO/update-recado.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 
 export class RecadosService {
+  constructor(
+    @InjectRepository(RecadoEntity)
+    private readonly recadoRepository: Repository<RecadoEntity>,
+  ) { }
+
   private lasrID = 1;
   private recados: RecadoEntity[] = [
-  {
-    id: 1,
-    texto: 'Este é um teste',
-    de: 'joana',
-    para: 'joao',
-    lido: false,
-    data: new Date(),
-  },
-];
+
+    {
+      id: 1,
+      texto: 'Este é um teste',
+      de: 'joana',
+      para: 'joao',
+      lido: false,
+      data: new Date(),
+    },
+  ];
 
 
-  findAll(){
-    return this.recados;
+  async findAll() {
+    const recados = await this.recadoRepository.find();
+    return recados;
   }
-  
-  throwNotFoundErro(){
+
+  throwNotFoundErro() {
     throw new HttpException("Recado Não emcotrado", HttpStatus.NOT_FOUND);
   }
-  
-  findOne(id: string){
+
+  findOne(id: string) {
     const recado = this.recados.find(item => item.id === +id)
 
     if (recado) return recado;
-    
-    this.throwNotFoundErro();
-  
-  }
-  
 
-  create(createRecado: CreateRecadoDto){
-    this.lasrID ++;
+    this.throwNotFoundErro();
+
+  }
+
+
+  create(createRecado: CreateRecadoDto) {
+    this.lasrID++;
     const id = this.lasrID;
     const novoRecado = {
       id,
-      ... createRecado,
+      ...createRecado,
       lido: false,
       data: new Date()
     };
     this.recados.push(novoRecado)
-    
+
     return novoRecado
   }
 
-  update(id: string, updateRecado: UpdateRecadoDto){
+  update(id: string, updateRecado: UpdateRecadoDto) {
     const recadoExistenteIndex = this.recados.findIndex(
       item => item.id === +id,
     )
-    if ( recadoExistenteIndex <= 0){
+    if (recadoExistenteIndex <= 0) {
       this.throwNotFoundErro()
     }
 
-      const recadoExistente = this.recados[recadoExistenteIndex];
-      this.recados[recadoExistenteIndex] = {
-        ...recadoExistente,
-        ...updateRecado,
+    const recadoExistente = this.recados[recadoExistenteIndex];
+    this.recados[recadoExistenteIndex] = {
+      ...recadoExistente,
+      ...updateRecado,
     }
   }
 
-  remove(id: string){
+  remove(id: string) {
     const recadoExistenteIndex = this.recados.findIndex(
       item => item.id === +id,
     );
-    if(recadoExistenteIndex <= 0){
+    if (recadoExistenteIndex <= 0) {
       this.throwNotFoundErro()
     }
-    
+
     const recado = this.recados[recadoExistenteIndex]
-      
+
     this.recados.splice(recadoExistenteIndex, 1);
-      
-      return recado
-    }
+
+    return recado
   }
+}
 
